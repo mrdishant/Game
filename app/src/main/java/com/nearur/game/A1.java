9c8d7aac1cc6a35ebfd351779eeba6f173e335d6
mrdishant <mr.dishantmahajan@gmail.com>
2017-07-08 14:28:01 +0530

    Initial commit

---
+++ app/src/main/java/com/nearur/game/A1.java
@@ -0,0 +1,31 @@
+package com.nearur.game;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class A1 extends AppCompatActivity {
+    EditText t;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a1);
+        t=(EditText)findViewById(R.id.editText);
+    }
+    public void cl(View v) {
+        Intent i = new Intent(this, A2.class);
+        if (t.getText().length() > 0) {
+            int n = Integer.parseInt(t.getText().toString().trim());
+            i.putExtra("number", n);
+            startActivity(i);
+        }
+        else{
+            Toast.makeText(this,"No Input",Toast.LENGTH_SHORT).show();
+        }
+    }
+}
+
+
---
+++ app/src/main/java/com/nearur/game/A2.java
@@ -0,0 +1,52 @@
+package com.nearur.game;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Scroller;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.PriorityQueue;
+import java.util.Set;
+
+public class A2 extends AppCompatActivity {
+    TextView t1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a2);
+        t1=(TextView)findViewById(R.id.textView2);
+        Intent r=getIntent();
+        int n=r.getIntExtra("number",0);
+        random(n);
+    }
+
+    public void random(int n){
+        ArrayList<Integer> d=new ArrayList<Integer>();
+        for(int i=1;i<=n*3;i++){
+            d.add(i);
+        }
+        StringBuffer b=new StringBuffer();
+        Collections.shuffle(d);
+        Iterator<Integer> itr=d.iterator();
+        int i=1,j=0;
+        while(itr.hasNext()){
+            if(j%3==0){
+                b.append("\n"+"Player "+i+" : ");
+                i++;
+            }
+            b.append(itr.next()+" ");
+            j++;
+        }
+
+
+        Scroller s=new Scroller(this);
+        t1.setScroller(s);
+        t1.setText(b);
+    }
+}
